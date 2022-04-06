ed020300908c9b25777e4f9e05c367d224172954
Makoto KUNO <mkt-kuno@iis.u-tokyo.ac.jp>
2022-04-07 08:43:46 +0900

    support GPU version

--- run.sh
+++
@@ -1,1 +0,0 @@
-docker run -it -d -p '50021:50021' --restart=unless-stopped hiroshiba/voicevox_engine:cpu-ubuntu20.04-latest
---
+++ run_cpu.sh
@@ -0,0 +1,1 @@
+docker run -it -d -p '50021:50021' --restart=unless-stopped --name voicevox_cpu voicevox/voicevox_engine:cpu-ubuntu20.04-latest
